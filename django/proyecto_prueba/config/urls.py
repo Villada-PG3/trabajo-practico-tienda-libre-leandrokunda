9f4b36ff192b8438ebb726c00baa030c230c3400
leandrokunda <l.kunda@itsv.edu.ar>
2026-08-03 11:49:41 +0000

    Agregado de productos y gategorias

--- django/proyecto_prueba/config/urls.py
+++ django/proyecto_prueba/config/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
 from miapp.views import ProductosTemplateView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("productos/", ProductosTemplateView.as_view())
 ]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # ... tus otras rutas aquí
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/proyecto_prueba/miapp/models.py
+++ django/proyecto_prueba/miapp/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique = True)
+    slug = models.SlugField(unique=True)
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ["nombre"]
+    def __str__(self):
+        return self.nombre
 class Producto(models.Model):
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE,
+        related_name="productos",
+        null=True,
+        blank=True,
+        )
     nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(max_digits=5, decimal_places=2)
-
+    descripcion = models.TextField(max_length=100, default=0)
+    precio = models.DecimalField(max_digits=100, decimal_places=2)
+    stock = models.IntegerField()
+    marca = models.CharField(max_length=50, default="Marca Desconocida")
+    imagen = models.ImageField(upload_to="productos/", null=True, blank=True)
     def __str__(self):
         return self.nombre
