850451bc1d569827c6ae379263243155f1754b38
leandrokunda <l.kunda@itsv.edu.ar>
2026-08-22 14:50:40 +0000

    feat: incorporar herencia de templates y Bootstrap

--- django/proyecto_prueba/miapp/admin.py
+++ django/proyecto_prueba/miapp/admin.py
@@ -3,5 +3,29 @@ from django.contrib import admin
 # Register your models here.
 from .models import Producto
 from .models import Categoria
-admin.site.register(Producto)
+from django.utils.html import format_html
+from django.contrib import admin
+from miapp.models import Producto, Categoria
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'mostrar_miniatura')
+    readonly_fields = ('mostrar_imagen_detalle',)
+    def mostrar_miniatura(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="width: 50px; height: 50px; object-fit: cover; border-radius: 4px;" />', obj.imagen.url)
+        return "Sin imagen"
+    
+    mostrar_miniatura.short_description = 'Miniatura'
+
+    def mostrar_imagen_detalle(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="max-width: 300px; max-height: 300px; border: 1px solid #ccc;" />', obj.imagen.url)
+        return "Sin imagen"
+
+    mostrar_imagen_detalle.short_description = 'Previsualización de la Imagen'
+
+
 admin.site.register(Categoria)
+# Register your models here.
