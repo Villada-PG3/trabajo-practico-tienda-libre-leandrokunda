1881e686eaebe29d796ee6a13ec115fa16a2d1ee
leandrokunda <l.kunda@itsv.edu.ar>
2026-08-31 11:32:05 +0000

    feat: template dinámico con DTL en home

--- django/proyecto_prueba/miapp/views.py
+++ django/proyecto_prueba/miapp/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.http import HttpResponse
-# Create your views here.c
+
 class ProductosTemplateView(TemplateView):
-    template_name = "productos.html"
+    template_name = "miapp/productos.html"
 
 def home(request):
-    return render(request, 'miapp/home.html')
+    productos_destacados = [
+        {"nombre": "Auriculares HP", "precio": 15000, "stock": 5},
+        {"nombre": "Teclado Mecanico", "precio": 45000, "stock": 0},
+        {"nombre": "Mouse Gamer", "precio": 12500, "stock": 12},
+        {"nombre": "Monitor 24 pulgadas", "precio": 120000, "stock": 3},
+        {"nombre": "Silla Gamer", "precio": None, "stock": 8},
+        {"nombre": "Mousepad XL", "precio": 5000, "stock": 20},
+    ]   
+    context = {
+        "titulo": "Ofertas de la semana",
+        "productos": productos_destacados,  # Sin comillas para pasar la lista
+        "usuario_logueado": True
+    }
+    # Ruta corregida a "miapp/home.html"
+    return render(request, "miapp/home.html", context)
 
 def acerca_de_mi(request):
     return render(request, 'miapp/acerca-de-mi.html')
